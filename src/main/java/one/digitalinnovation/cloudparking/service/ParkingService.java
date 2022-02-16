b88a5150151642b9764dfe339d0dcc1538273bde
elisio <elisiorcsilva@hotmail.com>
2022-02-16 11:31:59 -0300

    Adicionando Repository

--- src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/cloudparking/service/ParkingService.java
@@ -3,31 +3,37 @@ package one.digitalinnovation.cloudparking.service;
 
 import one.digitalinnovation.cloudparking.exception.ParkingNotFoundExcption;
 import one.digitalinnovation.cloudparking.model.Parking;
+import one.digitalinnovation.cloudparking.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
 
-    private static Map<String, Parking> parkingMap = new HashMap<>();
+    ParkingRepository parkingRepository;
 
-    static {
-        var id = getUUID();
-        var id1 = getUUID();
-        Parking parking = new Parking(id, "DMS-1111", "SC", "Celta", "Preto");
-        Parking parking1 = new Parking(id1, "AWS-2222", "SP", "Punto", "Cinza");
-        parkingMap.put(id, parking);
-        parkingMap.put(id1, parking1);
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
     }
 
+    //
+//    private static Map<String, Parking> parkingMap = new HashMap<>();
+//
+//    static {
+//        var id = getUUID();
+//        var id1 = getUUID();
+//        Parking parking = new Parking(id, "DMS-1111", "SC", "Celta", "Preto");
+//        Parking parking1 = new Parking(id1, "AWS-2222", "SP", "Punto", "Cinza");
+//        parkingMap.put(id, parking);
+//        parkingMap.put(id1, parking1);
+//    }
+
     public List<Parking> findAll() {
-        return parkingMap.values().stream().collect(Collectors.toList());
+//        return parkingMap.values().stream().collect(Collectors.toList());
+        return parkingRepository.findAll();
     }
 
     private static String getUUID() {
@@ -35,33 +41,42 @@ public class ParkingService {
     }
 
     public Parking findById(String id) {
-        Parking parking = parkingMap.get(id);
+        return parkingRepository.findById(id).orElseThrow(() ->
+                new ParkingNotFoundExcption(id));
 
-        if (parking == null) {
-            throw new ParkingNotFoundExcption(id);
-        }
+//        Parking parking = parkingMap.get(id);
+//        if (parking == null) {
+//            throw new ParkingNotFoundExcption(id);
+//        }
+//
+//        return parking;
 
-        return parking;
     }
 
     public Parking create(Parking parkingCreate) {
         String uuid = getUUID();
         parkingCreate.setId(uuid);
         parkingCreate.setEntryDate(LocalDateTime.now());
-        parkingMap.put(uuid, parkingCreate);
+//        parkingMap.put(uuid, parkingCreate);
+
+        parkingRepository.save(parkingCreate);
 
         return parkingCreate;
     }
 
     public void delete(String id) {
         Parking parking = findById(id);
-        parkingMap.remove(id);
+//        parkingMap.remove(id);
+
+        parkingRepository.delete(parking);
     }
 
     public Parking update(String id, Parking parkingCreate) {
         Parking parking = findById(id);
         parking.setColor(parkingCreate.getColor());
-        parkingMap.replace(id, parking);
+//        parkingMap.replace(id, parking);
+
+        parkingRepository.save(parking);
         return parking;
 
     }
